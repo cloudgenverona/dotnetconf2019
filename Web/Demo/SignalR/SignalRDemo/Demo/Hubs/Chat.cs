4c68dfdb9c045b0c7d68a20a4660404700fe8899
CloudGen <32154966+cloudgenverona@users.noreply.github.com>
2019-10-11 11:03:14 +0200

    signalr

---
+++ Web/Demo/SignalR/SignalRDemo/Demo/Hubs/Chat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Hubs
+{
+    public class Chat : Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.SendAsync("Send", message);
+        }
+    }
+}
